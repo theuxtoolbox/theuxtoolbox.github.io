74f31d60a15627a486e0a937ff98434cb781a91a
Jack Bush <bush.jack@gmail.com>
2023-03-30 16:37:10 +0100

    basic content included

---
+++ src/content/process.js
@@ -0,0 +1,59 @@
+export const process = [
+	{
+		name: "Forming a Team",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [],
+	},
+	{
+		name: "Exploratory Research",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [],
+	},
+	{
+		name: "Defining",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [],
+	},
+	{
+		name: "Ideating + Testing",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [
+			{
+				name: "Inclusive Design Cards",
+				why: "",
+				readyWhen: "",
+				finishedWhen: "",
+				link: "http://www.theuxtoolbox.com/inclusive-design-scenarios/",
+			},
+		],
+	},
+	{
+		name: "Documenting + Refining",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [],
+	},
+	{
+		name: "Delivery",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [],
+	},
+	{
+		name: "Measuring",
+		why: "",
+		readyWhen: "",
+		finishedWhen: "",
+		tools: [],
+	},
+];
--- src/pages/catalogue.js
+++ src/pages/catalogue.js
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import Helmet from "react-helmet";
 
+import { process } from "../content/process";
 import Layout from "../components/layout";
 
 const CataloguePage = () => {
@@ -9,6 +11,20 @@ const CataloguePage = () => {
       <Helmet title="Catalogue" />
       <main>
         <h1>Catalogue</h1>
+        {process.map((step) => (
+          <>
+            <h2 key={step.name}> {step.name} </h2>
+            <p>{step.readyWhen}</p>
+            <p>{step.finishedWhen}</p>
+            <ul>
+              {step.tools.map((tool) => (
+                <li key={tool.name}>
+                  <Link to={tool.link}>{tool.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        ))}
       </main>
     </Layout>
   );
